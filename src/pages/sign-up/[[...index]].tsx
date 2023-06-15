517f5f37a2a3328c2678447c757658c4fcef3768
Mounir <polomnr1@gmail.com>
2023-06-15 13:45:56 +0100

    add router

--- src/pages/sign-up/[[...index]].tsx
+++ src/pages/sign-up/[[...index]].tsx
@@ -1,5 +1,10 @@
 import { SignUp } from "@clerk/nextjs";
 
+
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div className='grid place-items-center min-h-screen'>
+      <SignUp />
+    </div>
+  );
 }
